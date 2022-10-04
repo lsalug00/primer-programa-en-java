1df2f7f4d3e24f71293ff95849a249233cc0da43
lsalug00 <lidiasaludes@gmail.com>
2022-10-04 10:51:45 +0200

    Imprime comida favorita

--- Prueva.java
+++ Prueva.java
@@ -2,5 +2,6 @@ public class Prueva{
 	public static void main(String args[]){
 		System.out.println("Saludes Gonzalez");
 		System.out.println("21, 2001");
+		System.out.println("Tortilla de patata");
 	}
 }
