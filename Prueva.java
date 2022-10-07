246c1991443a290d57f0c84b730c232ed8a54fa4
lsalug00 <lidiasaludes@gmail.com>
2022-10-07 12:10:46 +0200

    artista

--- Prueva.java
+++ Prueva.java
@@ -1,7 +1,7 @@
 public class Prueva{
 	public static void main(String args[]){
-		System.out.println("Saludes Gonzalez");
 		System.out.println("21, 2001");
 		System.out.println("Tortilla de patata");
+		System.out.println("Melendi");
 	}
 }
